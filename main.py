45f871b1ea6889391dda5146a729981687a46556
died <diedwitches@gmail.com>
2026-03-24 15:22:08 -0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import win32gui
+import win32con
 
+taskbar = win32gui.FindWindow("Shell_TrayWnd", None)
+
+style = win32gui.GetWindowLong(taskbar, win32con.GWL_EXSTYLE)
+win32gui.SetWindowLong(taskbar, win32con.GWL_EXSTYLE, style | win32con.WS_EX_LAYERED)
+
+# 0 = fully transparent, 255 = solid
+win32gui.SetLayeredWindowAttributes(taskbar, 0, 0, win32con.LWA_ALPHA)
